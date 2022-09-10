144f6ec45e718594441288e7536feefbd830bae1
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-09-10 16:17:19 +0200

    refactor mol.py

--- mol.py
+++ mol.py
@@ -1,125 +1,156 @@
 prefixe = ["0", "méth", "éth", "prop", "but", "pent", "hex", "hept", "oct"]
 suffixe = ["an", "yl"]
 
+# On définit la function principale du programme : on va l'appeller quand on aura tout défnit
+def main():
+    # On définit toutes les valeurs par défaut ici
+    nb_c = False
+    r_rami = False
+    nb_rami = False
+    r_gc = False
+    ui = False
+    n_prefixe = 0
 
-while True:
-    nb_c = int(input("Combien de C dans la molécule ? "))
+    # On définit une valeur qui indique si la boucle doit se répéter, on peut la changer plus tard si besoin
+    repeat = True
 
-    while True:
+    while repeat:
+        # On récupère le nombre de C dans la molécule
+
+        # On check si la valeur est celle par défaut
+        if nb_c == False:
+            nb_c = int(input("Combien de C dans la molécule ? "))
+
+        # Si la valeur n'est pas correct
         if nb_c > 8 or nb_c < 1:
+            # On dit que la valeur est pas bonne et on la remet à sa valeur par défaut
             print("Nombre incorrect, réessayer.")
+            nb_c = False
+            pass
 
-        else:
-            radical = prefixe[nb_c] + suffixe[0]
-            print(f"Radical : {radical}")
+        radical = prefixe[nb_c] + suffixe[0]
+        print(f"Radical : {radical}")
 
+        # On récupère le nombre de ramification
+
+        # On répète le même principe avec r_rami
+        if r_rami == False:
             r_rami = int(
                 input("Quel est le rang de la ramification (0 pour pas de rami) ? ")
             )
 
-            while True:
-                if r_rami < 0 or r_rami > nb_c:
-                    print("Nombre incorrect, réessayer.")
+        if r_rami < 0 or r_rami > nb_c:
+            print("Nombre incorrect, réessayer.")
+            r_rami = False
+            pass
 
-                else:
-                    nb_rami = int(
-                        input(
-                            "Combien de C dans la ramification ? (0 pour pas de rami) "
-                        )
-                    )
+        # On récupère le nombre de C dans la ramification si besoin
 
-                    while True:
-                        if nb_rami < 0:
-                            print("Nombre incorrect, réessayer.")
+        if r_rami != 0:
+            # On répète le même principe avec nb_rami
+            if nb_rami == False:
+                nb_rami = int(
+                    input("Combien de C dans la ramification ? ")
+                )
 
-                        else:
-                            if r_rami != 0:
-                                n_prefixe = prefixe[r_rami] + suffixe[1]
-                                print(f"{r_rami}-{n_prefixe}{radical}")
+            if nb_rami < 1:
+                print("Nombre incorrect, réessayer.")
+                nb_rami = False
+                pass
 
-                            elif r_rami == 0 or nb_rami == 0:
-                                n_prefixe = 0
+        # On calcule
+        
+        if r_rami != 0:
+            n_prefixe = prefixe[r_rami] + suffixe[1]
+            print(f"{r_rami}-{n_prefixe}{radical}")
 
-                            else:
-                                print("Erreur")
-                                pass
+        elif r_rami == 0 or nb_rami == 0:
+            n_prefixe = 0
 
-                            r_gc = int(
-                                input("Quel est le rang du groupe caractéristique ? ")
-                            )
+        else:
+            print("Erreur")
+            pass
 
-                            while True:
+        # On récupère le rang du groupe caractéristique
 
-                                def fonc():
-                                    print(
-                                        "Choisissez un nombre, en fonction du groupe caractéristique de la molécule étudiée :"
-                                    )
-                                    print("1. Hydroxyle : C liaison OH")
-                                    print("Carbonyles :")
-                                    print(
-                                        "    2 : Aldéhyde : C double liaison O, et C lisaion simple H (en fin de chaine)"
-                                    )
-                                    print(
-                                        "    3 : Cétone : C double liason O (en milieu ou fin de chaine)"
-                                    )
-                                    print(
-                                        "4. Carboxyle : C double liaison O, et C lisaion simple OH (en fin de chaine)"
-                                    )
+        if r_gc == False:
+            r_gc = int(
+                input("Quel est le rang du groupe caractéristique ? ")
+            )
 
-                                    while True:
-                                        choix = int(
-                                            input("Choisissez : 1, 2, 3, ou 4 : ")
-                                        )
+        if r_gc < 0 or r_gc > nb_c:
+            print("Nombre incorrect, réessayer.")
+            r_gc = False
+            pass
 
-                                        t = ["0", "ol", "al", "one", "oïque"]
+        elif r_gc == 1:
+            if ui == False:
+                ui = int(
+                    input("Est-ce que le gc est toujours en position 1 ? 1 pour oui, 2 pour non : ")
+                )
 
-                                        if choix not in [1, 2, 3, 4]:
-                                            print("Choix incorrect")
+            if ui not in [1, 2]:
+                print("Erreur")
+                ui == False
+                pass
+            
+            # j'ai juste supprimé un arrêt de la boucle inutile ici
 
-                                        elif choix == (1 or 2 or 3):
+        # On arrête la boucle mais pas le script
+        repeat = False
+    
+    # Ici j'ai juste remplacé la fonction par le contenu de cette dernière : ce bout n'est utilisé qu'une seule fois
 
-                                            if n_prefixe == 0:
-                                                print(f"{radical}-{r_gc}-{t[choix]}")
+    # J'ai juste modifié le style ici car je trouvais que ça prennait trop de place : si tu veux tout changer hésite pas
+    print("Choisissez un nombre, en fonction du groupe caractéristique de la molécule étudiée :")
+    print("1. Hydroxyle : C liaison OH")
+    print("Carbonyles :")
+    print("    2 : Aldéhyde : C double liaison O, et C lisaion simple H (en fin de chaine)")
+    print("    3 : Cétone : C double liason O (en milieu ou fin de chaine)")
+    print("    4. Carboxyle : C double liaison O, et C lisaion simple OH (en fin de chaine)")
 
-                                            elif (
-                                                (n_prefixe == 0)
-                                                and (r_gc == 1)
-                                                and (ui == 1)
-                                            ):
-                                                print(f"{radical}{t[choix]}")
+    # On réindique qu'on repeat de nouveau la boucle
+    repeat = True
 
-                                            else:
-                                                print(
-                                                    f"{r_rami}-{n_prefixe}{radical}-{r_gc}-{t[choix]}"
-                                                )
+    while repeat:
+        choix = int(
+            input("Choisissez : 1, 2, 3, ou 4 : ")
+        )
 
-                                        elif choix == 4:
+        t = ["0", "ol", "al", "one", "oïque"]
 
-                                            if (r_rami == 0) or (n_prefixe == 0):
-                                                print(f"Acide {radical}{t[4]}")
+        if choix not in [1, 2, 3, 4]:
+            print("Choix incorrect")
+            pass
 
-                                            else:
-                                                print(
-                                                    f"Acide-{r_rami}-{n_prefixe}{radical}{t[4]}"
-                                                )
-                                        quit()
+        elif choix == (1 or 2 or 3):
 
-                                if r_gc < 0 or r_gc > nb_c:
-                                    print("Nombre incorrect, réessayer.")
+            if n_prefixe == 0:
+                print(f"{radical}-{r_gc}-{t[choix]}")
 
-                                elif r_gc == 1:
-                                    ui = int(
-                                        input(
-                                            "Est-ce que le gc est toujours en position 1 ? 1 pour oui, 2 pour non : "
-                                        )
-                                    )
+            elif (
+                (n_prefixe == 0)
+                and (r_gc == 1)
+                and (ui == 1)
+            ):
+                print(f"{radical}{t[choix]}")
 
-                                    while True:
-                                        if ui not in [1, 2]:
-                                            print("Erreur")
-                                        else:
-                                            fonc()
-                                            quit()
-                                else:
-                                    fonc()
-                                    quit()
+            else:
+                print(
+                    f"{r_rami}-{n_prefixe}{radical}-{r_gc}-{t[choix]}"
+                )
+
+        # je l'ai juste mis en else car le choix ne peut être que "4"
+        else:
+            if (r_rami == 0) or (n_prefixe == 0):
+                print(f"Acide {radical}{t[4]}")
+
+            else:
+                print(
+                    f"Acide-{r_rami}-{n_prefixe}{radical}{t[4]}"
+                )
+        
+        # On stop la boucle
+        repeat = False
+
+main()
